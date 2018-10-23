a37bf893c183df7d11153246c0538bb02d21a5eb
pritam <pb457@njit.edu>
2018-10-23 00:39:38 -0400

    Task: Route get and post methods added to web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@home')->name('home');
+
+
+Route::get('/about', 'PageController@about' )->name('about');
+
+
+Route::get('/contact', 'PageController@contact')->name('contact');
+
+
+Route::post('/contact','PageController@store')->name('contact.store');
+
+
+Route::get('/thanks/{name}', 'PageController@thanks')->name('thanks');
