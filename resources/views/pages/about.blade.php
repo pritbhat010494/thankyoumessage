1ad1ae11b66b48e7adad27ebe7fbb5261d3c586c
pritam <pb457@njit.edu>
2018-10-23 00:35:29 -0400

    Task: Edited content of about & home pages

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <br>
+    <br>
+    <br>
+    <h1 style="color: black"><b>Hello, Welcome to NJIT</b><br>
+        The New Jersey Institute of Technology (NJIT) is a public research university in the University Heights neighborhood of Newark, New Jersey. Centrally located in the New York metropolitan area, its campus is within walking distance of Downtown Newark. New York City, 9 miles (14.5 km) and under 30 minutes away, is directly accessible from campus via rapid transit.[b] NJIT is New Jersey's Science & Technology University.Among other initiatives, it operates the Enterprise Development Center (EDC), one of the largest tech-oriented business incubators in the country.</h1>
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <br>
+    <br>
+    <br>
+    <h1 style="color:black">Hello, Welcome to NJIT<br>
+    NJIT MOODLE</h1>
+
+@endsection
